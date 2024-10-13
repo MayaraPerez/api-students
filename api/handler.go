9a56524b663a182b1b41528ad373e5f8d9637b6e
Mayara Perez <mayara.perez@g.globo>
2024-10-13 19:21:19 -0300

    creating file Handler on package API

---
+++ api/handler.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/MayaraPerez/api-students/db"
+	"github.com/labstack/echo/v4"
+)
+
+// Handlers
+func (api *API) GetStudents(c echo.Context) error {
+	students, err := api.DB.GetStudents()
+	if err != nil {
+		return c.String(http.StatusNotFound, "Error to List students")
+	}
+
+	return c.JSON(http.StatusOK, students)
+}
+
+func (api *API) createStudent(c echo.Context) error {
+	student := db.Student{}
+	if err := c.Bind(&student); err != nil {
+		return err
+	}
+	if err := api.DB.AddStudent(student); err != nil {
+		return c.String(http.StatusBadRequest, "Error to created student")
+	}
+	return c.String(http.StatusOK, "Created students!")
+}
+
+func (api *API) getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Get %s student", id)
+	return c.String(http.StatusOK, "Get of specific students!"+getStud)
+}
+
+func (api *API) updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, updateStud)
+}
+
+func (api *API) deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("Delete %s student", id)
+	return c.String(http.StatusOK, deleteStud)
+}
