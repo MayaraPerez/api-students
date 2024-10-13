76e61185d4aa5dec060b43ffa7f6cd30e819dea0
Mayara Perez <mayara.perez@g.globo>
2024-10-13 14:01:13 -0300

    adding rotas to api students

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // Routes
+  e.GET("/students", getStudents)
+  e.POST("/students", createdStudent)
+  e.GET("/students/:id", getStudent)
+  e.PUT("/students/:id", updateStudent)
+  e.DELETE("/students/:id", deleteStudent)
+
+  // Start server
+  e.Logger.Fatal(e.Start(":8080"))
+}
+
+// Handler
+func getStudents(c echo.Context) error {
+  return c.String(http.StatusOK, "List of all students!")
+}
+
+func createdStudent(c echo.Context) error {
+  return c.String(http.StatusOK, "Created students!")
+}
+
+func getStudent(c echo.Context) error {
+  id := c.Param("id")
+  getStud := fmt.Sprintf("Get %s student", id)
+  return c.String(http.StatusOK, "Get of specific students!"+ getStud)
+}
+
+func updateStudent(c echo.Context) error {
+  id := c.Param("id")
+  updateStud := fmt.Sprintf("Update %s student", id)
+  return c.String(http.StatusOK, updateStud)
+}
+
+func deleteStudent(c echo.Context) error {
+  id := c.Param("id")
+  deleteStud := fmt.Sprintf("Delete %s student", id)
+  return c.String(http.StatusOK, deleteStud)
+}
